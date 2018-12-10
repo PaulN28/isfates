dceffa14243d8f6586cc998d160a8dda99c50a00
NilsGamlin <31780810+NilsGamlin@users.noreply.github.com>
2018-12-10 20:18:30 +0100

    Add files via upload

---
+++ Neuer Ordner/Ueb06.java
@@ -0,0 +1,108 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+/**
+* Eine Klasse, die die Methoden der Uebung 6 beinhaltet
+*
+* @author Paul NICOLAS und Nils GAMLIN
+* @version 07/12/18
+*/
+
+public class Ueb06
+{
+    /**
+    * Methode, die zu einem übergebenen Feld mit double-Messwerten das arithmetische Mittel dieser Werte bestimmt,
+    * sowie den Messwert, der am nächsten daran liegt und den der am weitesten davon entfernt ist.
+    *
+    * @param  double[] Ein Feld mit double Messwerten
+    * @return double[] Ein Feld mit min, mittel und max
+    */
+    public static double[] messwerte (double[] tab) {
+      double mittel, min, max;
+      mittel = 0.0;
+      for (int i = 0; i < tab.length; i++) {
+        mittel += tab[i];
+      }
+      mittel = mittel / tab.length;
+      min = max = tab[0];
+      for (int i = 1; i < tab.length; i++) {
+        if (Math.abs(mittel - tab[i]) < Math.abs(mittel - min)) {
+          min = tab[i];
+        }
+        if (Math.abs(mittel - tab[i]) > Math.abs(mittel - max)) {
+          max = tab[i];
+        }
+      }
+      return new double[] {min, mittel, max };
+    }
+
+    public static void printMesswerte(double[] tab) {
+      System.out.println("\ndie Ergebnisswerte : min, mittel, max : \n\t" +
+                               java.util.Arrays.toString( messwerte(tab) ));
+    }
+
+    /**
+    * Methode die zu einem übergebenen String-Array bestimmt, wieviele Strings darin enthalten sind, die ausschließlich aus Klein- oder Großbuchstaben bestehen.
+    *
+    * @param String[] Array von Strings
+    * @return anzahl an Strings in der Tabelle, die ausschließlich aus Klein - oder Großbuchstaben bestehen.
+    */
+    public static int stringZaehler (String[] stab) {
+      int summe = 0;
+      for (String s : stab)
+          {
+           if ( s.matches( "[a-zA-Z]+" ) )
+             {
+              summe++;
+             }
+          }
+          return summe;
+      }
+
+    /**
+    * InsertionSort Algorithmus. Sortiert die Zahlen eines uebergebenen Arrays. (klein bis groß)
+    *
+    * @param  int[] array ist eine unsortierte/sortierte Liste von Zahlen
+    * @return insertionSort Sorted Array
+    */
+    public static int[] insertionSort(int[] array)
+    {
+        int aktuelleZahl;
+        int j;
+        int arrayLength = array.length;
+
+        for (int i = 1; i < arrayLength; i++) {
+            aktuelleZahl = array[i];
+            j = i-1;
+
+            while (j >= 0 && array[j] > aktuelleZahl) {
+                array[j+1] = array[j];
+                j = j-1;
+            }
+            array[j+1] = aktuelleZahl;
+        }
+        return array;
+    }
+
+    public static void printInsertionSort(int[] array) {
+      System.out.println(  "\n Das sortierte intArray : \n\t" +
+                                java.util.Arrays.toString(insertionSort( array )));
+    }
+
+    /**
+    * Main Methode fuer die Klasse Ueb06
+    * Es ist moeglich in den Feldern beliebig zu aendern
+    */
+    public static void main(String[] args) {
+      double messwerte_tab[] = {0.7, -8.6, -20, 0.8, 9};
+      String stringZaehler_tab[] = {"p1", "mml", "p    m", "%pa,", "v"};
+      int insertionSort_tab[]  = {2, 8, 5, 7, 1, 3, 15, -9};
+
+      printMesswerte(messwerte_tab);
+
+      System.out.println("\n\n " + stringZaehler(stringZaehler_tab));
+
+      printInsertionSort(insertionSort_tab);
+
+     }
+    
+}
---
+++ Neuer Ordner/Ueb06Test.java
@@ -0,0 +1,123 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class Ueb06Test.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class Ueb06Test
+{
+    private static final double MITTELWERT_PRAEZISION = 0.000001;
+    /**
+     * Default constructor for test class Ueb06Test
+     */
+    public Ueb06Test()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+
+    /************************************ Messwert Tests ****************************************************/
+    
+    @Test
+    public void test1Messwerte()
+    {
+        assertArrayEquals(new double[]{0.7, -3.62, -20}, Ueb06.messwerte(new double[]{0.7, -8.6, -20, 0.8, 9}), MITTELWERT_PRAEZISION);
+    }
+    
+    @Test
+    public void test2Messwerte()
+    {
+        assertArrayEquals(new double[]{3.5, 4.16, 9}, Ueb06.messwerte(new double[]{0.1, 0.2, 8, 3.5, 9}), MITTELWERT_PRAEZISION);
+    }
+    
+    @Test
+    public void test3Messwerte()
+    {
+        assertArrayEquals(new double[]{-4, -4.1, -8}, Ueb06.messwerte(new double[]{-4, -3, -4.8, -8, -0.7}), MITTELWERT_PRAEZISION);
+    }
+    
+    @Test
+    public void test4Messwerte()
+    {
+        assertArrayEquals(new double[]{-0.01, 0.061666667, -0.8}, Ueb06.messwerte(new double[]{0.7, 0.4, -0.01, -0.8, -0.25, 0.33}), MITTELWERT_PRAEZISION);
+    }
+    
+    /************************************** StringZaehler Tests **************************************************/
+    
+    @Test
+    public void test1stringZaehler()
+    {
+        assertTrue(Ueb06.stringZaehler(new String[]{"  dddd  ", "kdkdkfkd  dfk", "dd5", "§d!?", "SnSu"}) == 1);
+    }
+    
+    
+    @Test
+    public void test2stringZaehler()
+    {
+        assertFalse(Ueb06.stringZaehler(new String[]{"BvB", "Meisterschaft", "gewinnen", "MR10", "Sancho"}) == 5);
+    }
+    
+    @Test
+    public void test3stringZaehler()
+    {
+        assertTrue(Ueb06.stringZaehler(new String[]{"______", "      ", "§$%&/()", "854245", "MiaSanMia"}) == 1);
+    }
+    
+    @Test
+    public void test4stringZaehler()
+    {
+        assertFalse(Ueb06.stringZaehler(new String[]{"dk:W", "...g", "4öüüöüö", "öüöüäüö", "Götze"}) == 2);
+    }
+    
+    /************************************** InsertionSort Tests **************************************************/
+    
+    @Test
+    public void test1InsertionSort()
+    {
+        assertArrayEquals(new int[]{1, 2, 3, 4}, Ueb06.insertionSort(new int[]{1, 2, 3, 4}));
+    }
+                                                                 
+    @Test
+    public void test2InsertionSort()
+    {
+        assertArrayEquals(new int[]{-1, 2, 15, 16}, Ueb06.insertionSort(new int[]{-1, 15, 16, 2}));
+    }
+    
+    @Test
+    public void test3InsertionSort()
+    {
+        assertArrayEquals(new int[]{-20, -16, -15, -1}, Ueb06.insertionSort(new int[]{-1, -15, -16, -20}));
+    }
+    
+    @Test
+    public void test4InsertionSort()
+    {
+        assertArrayEquals(new int[]{-100, 7, 10, 20, 560}, Ueb06.insertionSort(new int[]{20, 10, -100, 7, 560}));
+    }
+}
+
