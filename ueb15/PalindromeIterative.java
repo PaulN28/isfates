4bc531c0d07d388ba064d97e4ca5d3c44c8802dd
PaulN28 <31801255+PaulN28@users.noreply.github.com>
2019-04-22 19:05:20 +0200

    Add files via upload

---
+++ ueb15/PalindromeIterative.java
@@ -0,0 +1,61 @@
+
+/**
+ * Décrivez votre classe PalindromeIterative ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+
+import java.io.*;
+
+public class PalindromeIterative
+{
+  public static boolean isPalindromeIterative (String test) {
+
+    int testLength = test.length();
+
+    for (int i = 0; i < (testLength / 2) + 1; i++){
+
+      if (testLength < 2) {
+        return true;
+      }
+
+      if (test.charAt(i) != test.charAt(testLength - i  - 1)){
+        return false;
+      }
+  }
+  return true;
+}
+
+
+  public void start (String dateiName)
+  throws IOException
+  {
+
+    File datei = new File(dateiName);
+    BufferedReader reader = new BufferedReader (new FileReader(datei));
+    String aktuelleZeile;
+
+    while ((aktuelleZeile = reader.readLine()) != null){
+
+      System.out.println(isPalindromeIterative(aktuelleZeile));
+    }
+    reader.close();
+  }
+
+  public static void main(String[] args) {
+      for (int i = 0; i < args.length; i++)
+    {
+      String dateiName = new String(args[i]);
+      System.out.println ( "\n\t Datei : " + dateiName);
+        try {
+            if (args.length > 0) {
+                PalindromeIterative palindromeiterative = new PalindromeIterative();
+                palindromeiterative.start(dateiName);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
+}
