e71b17a742042fbd459708663d5075e6cd6f0e11
NilsGamlin <31780810+NilsGamlin@users.noreply.github.com>
2019-05-07 20:08:08 +0200

    comment

--- Average.java
+++ Average.java
@@ -1,13 +1,11 @@
- 
-
 import java.util.Random;
 import java.util.Arrays;
 
 /**
- * Write a description of class Average here.
+ * Die Klasse Average ermöglicht es, ohne Anonyme Klasse, den Durchschnitt aller Werte eines gegebenen Arrays zu berechnen.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Paul Nicolas u. Nils Gamlin
+ * @version 03.05.2019
  */
 public class Average
 {
@@ -15,6 +13,11 @@ public class Average
     {
     }
     
+ /**
+ * Berechnet den Durchschnitt aller Werte und speichert den berechenten Wert an Stelle des groessten Werts im Array.
+ *
+ * @param float[] Array wo Durchschnitt berechnet wird.
+ */
     public static void average(float[] floatArray){
         int arrayGroesse = floatArray.length;
         System.out.println("Before average: " + Arrays.toString(floatArray));
@@ -31,6 +34,12 @@ public class Average
 
         System.out.println("After average: " + Arrays.toString(floatArray));
     }
+ 
+ /**
+ * Findet den Index des groessten Werts im Array.
+ *
+ * @param float[] Array durch welches iteriert wird.
+ */
 
     public static int getMaxIndex(float[] floatArray){
         int indexMax = 0;
