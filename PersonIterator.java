3bc12b5e61fb9c579446a2bf7b9d0292206908b9
NilsGamlin <31780810+NilsGamlin@users.noreply.github.com>
2019-05-07 20:54:35 +0200

    comment

--- PersonIterator.java
+++ PersonIterator.java
@@ -1,9 +1,10 @@
 /**
- * Write a description of interface PersonIterator here.
+ * Interface der es ermoeglicht java.util.Iterator<Person> Lokal zu erweitern.
  *
  * @author Nils Gamlin
  * @version 03.05.2019
  */
+
 public interface PersonIterator extends java.util.Iterator<Person> {
     
 }
