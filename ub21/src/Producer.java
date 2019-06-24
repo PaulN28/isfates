45f3ace52e520ec797c530b8d04aef407dd15c2b
NilsGamlin <31780810+NilsGamlin@users.noreply.github.com>
2019-06-24 19:21:18 +0200

    Comment

--- ub21/src/Producer.java
+++ ub21/src/Producer.java
@@ -3,6 +3,11 @@ import java.util.Random;
 public class Producer {
 
     private Random ran;
+    
+    /**
+    * Erzeugt einen zufaelligen Integer zwischen 0 und 1000. Und gibt diesen zurück.
+    *
+    * @param int produzierte Zahl.
 
     public int produce(){
         int product = ran.nextInt(1000);
